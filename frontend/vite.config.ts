eead26c09110ac07cf5bb1e40c2c85a39c55ceb4
arjenou <wangyunjie1101@gmail.com>
2025-06-12 22:30:31 +0900

    fix banckend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://xinhangdao-api.wangyunjie1101.workers.dev')
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://xinhangdao-api.wangyunjie1101.workers.dev/api')
   }
 })
